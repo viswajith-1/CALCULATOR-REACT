f919d927b9332a9200e5f80bd89fcca28f5b0f88
Viswajith SB <viswajithsaji@gmail.com>
2024-09-24 19:17:16 +0530

    final stage

--- src/assets/button.jsx
+++ src/assets/button.jsx
@@ -1,3 +1,6 @@
+import { useContext } from "react"
+import { CalcContext } from "./CalcContext"
+
 const getStyleName = btn => {
   const className = {
     '=': 'equals',
@@ -10,8 +13,52 @@ const getStyleName = btn => {
 }
 
 const Button = ({ value }) => {
+
+  const { setCalc } =useContext(CalcContext);
+  console.log(setCalc);
+
+  const commaClick = () => {
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes ? calc.num + value : calc.num
+    })
+  }
+
+  const resetClick = () => {
+    setCalc({ sign: '', num: 0, res: 0})
+  }
+
+  const handleClickButton = () => {
+    const numberString = value.toString()
+    let numberValue;
+    if(numberString === '0' && calc.num === 0) {
+      numberValue= "0"
+    }
+    else{
+      numberValue= Number(calc.num + numberString)
+    }
+    setCalc({
+      ...calc,
+      num: numberValue
+    })
+  }
+
+  const handleBtnClick= () => {
+    console.log(value)
+    const results ={
+      '.': commaClick,
+      'C': resetClick
+    }
+    if(results[value]){
+      return results[value]()
+    }
+    else{
+      return handleClickButton()
+    }
+  }
+
   return (
-    <button className={`${getStyleName(value)} button`}>{value}</button>
+    <button onClick={handleBtnClick} className={`${getStyleName(value)} button`}>{value}</button>
   )
 }
 
