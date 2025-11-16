43e72e69c9d1175a8e88a8a3889182d3d4808a55
Viswajith S B <121398392+viswajith-1@users.noreply.github.com>
2025-11-17 00:36:08 +0530

    Update screen.jsx

--- src/assets/screen.jsx
+++ src/assets/screen.jsx
@@ -1,13 +1,13 @@
 import { useContext } from "react"
 import { CalcContext } from "./CalcContext"
-import { Textfit } from 'react-textfit';
+
 
 const Screen = () => {
   const { calc } = useContext(CalcContext);
     return (
-    <Textfit className="screen" max={70} mode="single">
+    <div className="screen">
       {calc.num ? calc.num : calc.res}
-    </Textfit>
+    </div>
   )
 }
 
